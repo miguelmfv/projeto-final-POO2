ac97cd69486a4bf5b7cd81fce303e7b59be3d33c
Miguel F. Vivan <100147829+miguelmfv@users.noreply.github.com>
2024-11-27 21:07:35 -0300

    aaa

---
+++ src/main/java/org/example/Carro.java
@@ -0,0 +1,82 @@
+package org.example;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Carro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    private String marca;
+    private String modelo;
+    private String cor;
+    private String placa;
+    private int ano;
+    private List<Carro> listCarros = new ArrayList<>();
+
+    @OneToMany(mappedBy = "carro", cascade = CascadeType.ALL)
+
+
+
+    public Carro(String marca, String modelo, String cor, String placa, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.placa = placa;
+        this.ano = ano;
+    }
+
+    public List<Carro> listCarros() {return listCarros;}
+}
---
+++ src/main/java/org/example/Usuario.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    private String email;
+    private String senha;
+    private String telefone;
+
+    public Usuario(String nome, String email, String senha, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.telefone = telefone;
+    }
+}
